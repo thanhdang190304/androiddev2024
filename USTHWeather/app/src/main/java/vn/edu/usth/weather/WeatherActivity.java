9866b8747fb2aa696d9665be8aa929fa93556da2
thanhdang190304 <thanh190304@gmail.com>
2024-09-13 15:22:42 +0700

    prac 6 updated

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,54 +1,46 @@
 package vn.edu.usth.weather;
-
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.util.Log;
 public class WeatherActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_forecast, firstFragment).commit();
+        WeatherFragment secondFragment = new WeatherFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_weather, secondFragment).commit();
+        Log.i("create", "onCreate called");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "=== APP STARTED ===");
+        Log.i("start", "onStart called");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "=== APP RESUMED ===");
+        Log.i("resume", "onResume called");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "=== APP PAUSED ===");
+        Log.i("pause", "onPause called");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "=== APP STOPPED ===");
+        Log.i("stop", "onStop called");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "=== APP DESTROYED ===");
+        Log.i("destroy", "onDestroy called");
     }
-
 }
