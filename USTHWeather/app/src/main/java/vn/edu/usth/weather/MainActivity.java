1375c695932c390af569d1814c2a839974618d83
thanhdang190304 <thanh190304@gmail.com>
2024-09-18 20:48:53 +0700

    prac 8

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -1,24 +1,45 @@
 package vn.edu.usth.weather;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.util.Log;
+public class WeatherActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "WeatherActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_weather);
+        Log.i(TAG, "=== APP CREATED ===");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "=== APP STARTED ===");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "=== APP RESUMED ===");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "=== APP PAUSED ===");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "=== APP STOPPED ===");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "=== APP DESTROYED ===");
     }
 }
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,21 +1,28 @@
 package vn.edu.usth.weather;
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.google.android.material.tabs.TabLayout;
 import android.util.Log;
+
 public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_forecast, firstFragment).commit();
-        WeatherFragment secondFragment = new WeatherFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_weather, secondFragment).commit();
-        Log.i("create", "onCreate called");
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        WeatherAndForecastPagerAdapter adapter = new WeatherAndForecastPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Optionally, set tab titles
+        tabLayout.getTabAt(0).setText("Viet Nam");
+        tabLayout.getTabAt(1).setText("France");
+        tabLayout.getTabAt(2).setText("India");
     }
 
     @Override
@@ -36,11 +43,4 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("stop", "onStop called");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("destroy", "onDestroy called");
-    }
-}
+    }}
