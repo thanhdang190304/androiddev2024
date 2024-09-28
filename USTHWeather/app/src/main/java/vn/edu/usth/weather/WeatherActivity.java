67ecf4b5f8f60d9d4cb0a8f095776956bffdde0f
thanhdang190304 <thanh190304@gmail.com>
2024-09-28 14:44:38 +0700

    prac 11 done

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,46 +1,118 @@
 package vn.edu.usth.weather;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.google.android.material.tabs.TabLayout;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+import android.media.MediaPlayer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        WeatherAndForecastPagerAdapter adapter = new WeatherAndForecastPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(adapter);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(viewPager);
+        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
 
-        // Optionally, set tab titles
-        tabLayout.getTabAt(0).setText("Viet Nam");
-        tabLayout.getTabAt(1).setText("France");
-        tabLayout.getTabAt(2).setText("India");
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
+
+        extractAndPlayMusic();
+
+        /*
+        //Practical 3: add code
+        // Create a new Fragment to be placed in the activity l
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.fragment_container, firstFragment).commit();
+
+        // Practical 6: add WeatherFragment
+        WeatherFragment secondFragment = new WeatherFragment();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.fragment_weather, secondFragment).commit();
+        */
+        Log.i("onCreate", "onCreate");
     }
 
+    private void extractAndPlayMusic() {
+        try {
+            // Copy to ext storage
+            InputStream inputStream = getResources().openRawResource(R.raw.music);
+            File musicFile = new File(getExternalFilesDir(null), "music.mp3");
+            OutputStream outputStream = new FileOutputStream(musicFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0){
+                outputStream.write(buffer, 0, length);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            // Play
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(musicFile.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // Practical 2
     @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
-        Log.i("start", "onStart called");
+        Log.i("onStart", "onStart");
     }
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
-        Log.i("resume", "onResume called");
+        Log.i("onResume", "onResume");
     }
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
-        Log.i("pause", "onPause called");
+        Log.i("onPause", "onPause");
     }
     @Override
-    protected void onStop() {
+    public void onStop() {
         super.onStop();
-    }}
+        Log.i("onStop", "onStop");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("onDestroy", "onDestroy");
+    }
+
+}
